fff58b5f8d9d94cfdb6afa721432de4aa220d614
Jordan <edmondsj@aston.ac.uk>
2018-03-20 15:34:33 +0000

    Test commit

--- app/src/main/java/com/example/jordan/ukelectionsapp/FlipFragment.java
+++ app/src/main/java/com/example/jordan/ukelectionsapp/FlipFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * {@link FlipFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link FlipFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class FlipFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class LoginFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public LoginFragment() {
+    public FlipFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment FlipFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static FlipFragment newInstance(String param1, String param2) {
+        FlipFragment fragment = new FlipFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_flip, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/jordan/ukelectionsapp/MainActivity.java
+++ app/src/main/java/com/example/jordan/ukelectionsapp/MainActivity.java
@@ -1,51 +1,32 @@
 package com.example.jordan.ukelectionsapp;
 
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FlipFragment.OnFragmentInteractionListener {
 
-    private EditText niNumberEditText;
-    private TextView niTextView;
-
-    private EditText passText;
-    private TextView passTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
-
-        /* Initializing views */
-        niNumberEditText = (EditText) findViewById(R.id.ni_field);
-        niTextView = (TextView) findViewById(R.id.show_ni);
-        niTextView.setVisibility(View.GONE);
-
-        /* Set Text Watcher listener */
-        niNumberEditText.addTextChangedListener(passwordWatcher);
+        setContentView(R.layout.fragment_flip);
 
     }
 
-    private final TextWatcher passwordWatcher = new TextWatcher() {
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            niTextView.setVisibility(View.VISIBLE);
-        }
-
-        public void afterTextChanged(Editable s) {
-            if (s.length() == 0) {
-                niTextView.setVisibility(View.GONE);
-            } else{
-                niTextView.setText("You have entered : " + niNumberEditText.getText());
-            }
-        }
-    };
+    @Override
+    public void onFragmentInteraction(Uri uri) { }
 }
